1760f47759c3c43e914f801c6639f37b34763870
Emroze <emrozesd@users.noreply.github.com>
2019-03-21 19:58:16 -0700

    added forums html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -22,6 +22,18 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/angels", function(req, res) {
+    res.render("angels");
+  });
+
+  app.get("/beggars", function(req, res) {
+    res.render("beggars");
+  });
+
+  app.get("/nosey", function(req, res) {
+    res.render("nosey");
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
