68976dabb48f0fe3771a7f01a85fe93cd95aea13
Calberg89 <chris_alberg@yahoo.com>
2019-03-23 13:24:04 -0700

    Updated Post Model

--- models/schema.sql
+++ models/schema.sql
@@ -1,26 +1,29 @@
 DROP DATABASE IF EXISTS blockPartyDb;
 CREATE DATABASE blockPartyDb;
 
-CREATE TABLE angels
+CREATE TABLE "angels"
 (
-    id int AUTO_INCREMENT,
-    message varchar(250) NOT NULL,
-    userName varchar(30) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+    'id' int AUTO_INCREMENT,
+    'message' varchar(250) NOT NULL,
+    'userName' varchar(30) NOT NULL,
+    'created_at' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ('id')
+);
 
-CREATE TABLE beggars
+CREATE TABLE "beggars"
 (
-    id int AUTO_INCREMENT,
-    message varchar(250) NOT NULL,
-    userName varchar(30) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+    'id' int AUTO_INCREMENT,
+    'message' varchar(250) NOT NULL,
+    'userName' varchar(30) NOT NULL,
+    'created_at' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ('id')
+);
 
-CREATE TABLE nosey
+CREATE TABLE "nosey"
 (
-    id int AUTO_INCREMENT,
-    message varchar(250) NOT NULL,
-    userName varchar(30) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+    'id' int AUTO_INCREMENT,
+    'message' varchar(250) NOT NULL,
+    'userName' varchar(30) NOT NULL,
+    'created_at' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ('id')
+);
